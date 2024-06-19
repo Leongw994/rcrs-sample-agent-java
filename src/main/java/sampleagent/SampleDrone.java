fdb6843241ee7f88f83457c8119d09e58e5cce83
Leongw994 <leongw99@gmail.com>
2024-06-19 16:26:39 +0100

    Changes made for kobe map

--- src/main/java/sampleagent/SampleDrone.java
+++ src/main/java/sampleagent/SampleDrone.java
@@ -32,11 +32,8 @@ import sample.DistanceSorter;
 public class SampleDrone extends AbstractSampleAgent<Drone> {
 
     private static final Logger LOG = Logger.getLogger(SampleDrone.class);
-    private static final int VISION_RANGE = 500;
     private Collection<EntityID> unexploredBuildings;
 
-
-    private int distance;
 
 
     @Override
@@ -49,8 +46,7 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
         super.postConnect();
         model.indexClass(StandardEntityURN.CIVILIAN, StandardEntityURN.BUILDING,
                 StandardEntityURN.RESCUE_ROBOT, StandardEntityURN.DRONE,
-                StandardEntityURN.REFUGE, StandardEntityURN.HYDRANT,
-                StandardEntityURN.FIRE_BRIGADE, StandardEntityURN.GAS_STATION);
+                StandardEntityURN.REFUGE, StandardEntityURN.POLICE_OFFICE);
         unexploredBuildings = new HashSet<EntityID>(buildingIDs);
     }
 
@@ -82,7 +78,6 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
                 List<EntityID> path = search.breadthFirstSearch(me().getPosition(), next.getPosition());
                 if(path != null){
                     LOG.info("Moving to target");
-//                    sendMove(time, path);
                     // fly command
                     sendFly(time, path);
                     return;
@@ -94,32 +89,17 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
         List<EntityID> path = search.breadthFirstSearch(me().getPosition(), unexploredBuildings);
         if(path != null) {
             LOG.info("Searching map");
-//            sendMove(time, path);
             sendFly(time, path);
             return;
         }
         LOG.info("Flying in random direction");
-        //sendMove(time, randomWalk());
-        sendFly(time, randomWalk());
+        sendFly(time, randomFly());
     }
-
-
 
     @Override
     protected EnumSet<StandardEntityURN> getRequestedEntityURNsEnum() {
         return EnumSet.of(StandardEntityURN.DRONE);
     }
-
-/*//    private void goThroughBlockade(int time, Blockade blockade) {
-//        Collection<StandardEntity> roads = model.getEntitiesOfType(StandardEntityURN.ROAD);
-//        List<EntityID> res = new ArrayList<EntityID>();
-//        for (StandardEntity next : roads) {
-//
-//        }
-////        sendClear(time, blockade.getID());
-////        sendMove(time, randomWalk());
-//
-//    }*/
 
     private void sendCoordinatesToPolice(int time, int x, int y) {
         Collection<StandardEntity> entities = model.getEntitiesOfType(StandardEntityURN.RESCUE_ROBOT);
@@ -135,9 +115,9 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
         for (StandardEntity next: model.getEntitiesOfType(
             StandardEntityURN.CIVILIAN)) {
                 Human human = (Human) next;
-                if(human == me()) {
-                    continue;
-                }
+//                if(human == me()) {
+//                    continue;
+//                }
                 if (human.isHPDefined() && human.isBuriednessDefined() && human.isDamageDefined()
                     && human.isPositionDefined() && human.getHP() > 0 && (human.getBuriedness() > 0 || human.getDamage() > 0)) {
                         targets.add(human);
@@ -151,82 +131,6 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
         for(EntityID next : changed.getChangedEntities()) {
             unexploredBuildings.remove(next);
         }
-    }
-
-    private List<EntityID> getBlockedRoads() {
-        Collection<
-                StandardEntity> e = model.getEntitiesOfType(StandardEntityURN.ROAD);
-        List<EntityID> result = new ArrayList<EntityID>();
-        for (StandardEntity next : e) {
-            Road r = (Road) next;
-            if (r.isBlockadesDefined() && !r.getBlockades().isEmpty()) {
-                result.add(r.getID());
-            }
-        }
-        return result;
-    }
-
-    private int findDistanceTo(Blockade b, int x, int y) {
-        // Logger.debug("Finding distance to " + b + " from " + x + ", " + y);
-        List<Line2D> lines = GeometryTools2D.pointsToLines(
-            GeometryTools2D.vertexArrayToPoints(b.getApexes()), true);
-        double best = Double.MAX_VALUE;
-        Point2D origin = new Point2D(x, y);
-        for (Line2D next : lines) {
-          Point2D closest = GeometryTools2D.getClosestPointOnSegment(next, origin);
-          double d = GeometryTools2D.getDistance(origin, closest);
-          // Logger.debug("Next line: " + next + ", closest point: " + closest + ",
-          // distance: " + d);
-          if (d < best) {
-            best = d;
-            // Logger.debug("New best distance");
-          }
-    
-        }
-        return (int) best;
-      }
-
-    private Blockade getTargetBlockade() {
-        LOG.debug("Looking for target blockade");
-        Area location = (Area) location();
-        LOG.debug("Looking in current location");
-        Blockade result = getTargetBlockade(location, distance);
-        if (result != null) {
-            return result;
-        }
-        LOG.debug("Looking in neighboring locations");
-        for (EntityID next : location.getNeighbours()) {
-            location = (Area) model.getEntity(next);
-            result = getTargetBlockade(location, distance);
-            if (result != null) {
-                return result;
-            }
-        }
-        return null;
-    }
-
-
-    private Blockade getTargetBlockade(Area area, int maxDistance) {
-        // Logger.debug("Looking for nearest blockade in " + area);
-        if (area == null || !area.isBlockadesDefined()) {
-            // Logger.debug("Blockades undefined");
-            return null;
-        }
-        List<EntityID> ids = area.getBlockades();
-        // Find the first blockade that is in range.
-        int x = me().getX();
-        int y = me().getY();
-        for (EntityID next : ids) {
-            Blockade b = (Blockade) model.getEntity(next);
-            double d = findDistanceTo(b, x, y);
-            // Logger.debug("Distance to " + b + " = " + d);
-            if (maxDistance < 0 || d < maxDistance) {
-                // Logger.debug("In range");
-                return b;
-            }
-        }
-        // Logger.debug("No blockades in range");
-        return null;
     }
   
  
