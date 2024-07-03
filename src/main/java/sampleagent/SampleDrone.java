fc3059e1119d1a70ba6fecebd0b25126c19be765
Luis Gustavo Nardin <gnardin@users.noreply.github.com>
2024-07-03 17:46:07 +0200

    Change randomWalk (#1)
    new commit

--- src/main/java/sampleagent/SampleDrone.java
+++ src/main/java/sampleagent/SampleDrone.java
@@ -6,23 +6,17 @@ import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
-// import java.util.Set;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
-// import org.dom4j.Entity;
-
-// import com.vividsolutions.jts.geom.impl.PackedCoordinateSequence.Double;
 
 import org.jfree.chart.block.Block;
 import rescuecore2.messages.Command;
 import rescuecore2.misc.geometry.GeometryTools2D;
 import rescuecore2.misc.geometry.Line2D;
 import rescuecore2.misc.geometry.Point2D;
-// import rescuecore2.misc.geometry.Vector2D;
-// import rescuecore2.standard.entities.AmbulanceTeam;
 import rescuecore2.misc.geometry.Vector2D;
 import rescuecore2.standard.entities.*;
-// import rescuecore2.standard.entities.StandardPropertyURN;
 import rescuecore2.worldmodel.ChangeSet;
 import rescuecore2.worldmodel.Entity;
 import rescuecore2.worldmodel.EntityID;
@@ -32,9 +26,10 @@ import sample.DistanceSorter;
 public class SampleDrone extends AbstractSampleAgent<Drone> {
 
     private static final Logger LOG = Logger.getLogger(SampleDrone.class);
+
+    private static final int    RANDOM_FLY_LENGTH = 2;
+  
     private Collection<EntityID> unexploredBuildings;
-
-
 
     @Override
     public String toString() {
@@ -44,10 +39,10 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
     @Override
     protected void postConnect() {
         super.postConnect();
-        model.indexClass(StandardEntityURN.CIVILIAN, StandardEntityURN.FIRE_BRIGADE,
-                StandardEntityURN.POLICE_FORCE, StandardEntityURN.AMBULANCE_TEAM,
-                StandardEntityURN.REFUGE, StandardEntityURN.HYDRANT,
-                StandardEntityURN.GAS_STATION, StandardEntityURN.BUILDING);
+        model.indexClass(StandardEntityURN.FIRE_BRIGADE,
+                         StandardEntityURN.POLICE_FORCE,
+                         StandardEntityURN.AMBULANCE_TEAM,
+                         StandardEntityURN.BUILDING);
         unexploredBuildings = new HashSet<EntityID>(buildingIDs);
     }
 
@@ -62,6 +57,7 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
         }
         updateUnexploredBuildings(changed);
         //go through targets and see if there are any civilians
+        /*
         for (Human next : getTargets()) {
             if(next.getPosition().equals(location().getID())) {
                 //Target civilians that might need rescuing
@@ -73,7 +69,7 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
                         //Send coordinates to police office
                         sendCoordinatesToPolice(1, x, y);
                         return;
-                    }
+                }
             } else {
                 //try to move to target
                 List<EntityID> path = search.breadthFirstSearch(me().getPosition(), next.getPosition());
@@ -93,9 +89,9 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
             LOG.info("Searching map");
             sendFly(time, path);
             return;
-        }
+            }*/
         LOG.info("Flying in random direction");
-        sendFly(time, randomFly());
+        sendFly(time, randomWalk());
     }
 
     @Override
@@ -103,6 +99,34 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
         return EnumSet.of(StandardEntityURN.DRONE);
     }
 
+    @Override
+    protected List<EntityID> randomWalk() {
+      List<EntityID> result = new ArrayList<EntityID>( RANDOM_FLY_LENGTH );
+      Set<EntityID> seen = new HashSet<EntityID>();
+      EntityID current = ( (Robot) me() ).getPosition();
+      
+      for (int i = 0; i < RANDOM_FLY_LENGTH; ++i) {
+        result.add( current );
+        seen.add( current );
+        List<EntityID> possible = new ArrayList<EntityID>( neighbours.get( current ) );
+        Collections.shuffle(possible, random);
+        boolean found = false;
+        for ( EntityID next : possible ) {
+          //if ( seen.contains( next )) {
+          //  continue;
+          //}
+          current = next;
+          found = true;
+          break;
+        }
+        if (!found) {
+          //dead end
+          break;
+        }
+      }
+
+      return result;
+    }
 
     private void sendCoordinatesToPolice(int time, int x, int y) {
         Collection<StandardEntity> entities = model.getEntitiesOfType(StandardEntityURN.RESCUE_ROBOT);
@@ -118,9 +142,6 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
         for (StandardEntity next: model.getEntitiesOfType(
             StandardEntityURN.CIVILIAN)) {
                 Human human = (Human) next;
-//                if(human == me()) {
-//                    continue;
-//                }
                 if (human.isHPDefined() && human.isBuriednessDefined() && human.isDamageDefined()
                     && human.isPositionDefined() && human.getHP() > 0 && (human.getBuriedness() > 0 || human.getDamage() > 0)) {
                         targets.add(human);
@@ -134,7 +155,5 @@ public class SampleDrone extends AbstractSampleAgent<Drone> {
         for(EntityID next : changed.getChangedEntities()) {
             unexploredBuildings.remove(next);
         }
-    }
-  
- 
+    } 
 }
